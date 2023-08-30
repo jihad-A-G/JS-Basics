11a7cffc6db1eb347da286da34daf3d4f326fa1f
jihad-A-G <jihadabdlghani73@gmail.com>
2023-08-30 09:17:04 +0300

    Completed step 7

---
+++ javascript-basics-01/step_7.js
@@ -0,0 +1,5 @@
+let shoesSize,birthYear;
+shoesSize=prompt("Enter your shoes size: ");
+birthYear=prompt("Enter your birth year: ");
+shoesSize=(shoesSize * 2 + 5)*50 -birthYear +1766;
+alert("The result: "+shoesSize);
