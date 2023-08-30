c2414fb8de644ad3bb85b79d7289a9f00a092ab1
jihad-A-G <jihadabdlghani73@gmail.com>
2023-08-30 10:52:56 +0300

    Completed step 5

---
+++ javascript-basics-02/step 5/step-5.js
@@ -0,0 +1,8 @@
+const imgs=document.querySelectorAll("img");
+let num;
+for(let img in imgs){
+
+    imgs[img].addEventListener("mouseover",()=>{
+        imgs[img].setAttribute("src",`images/image${+img+1}_2.jpg`);
+    })
+}
